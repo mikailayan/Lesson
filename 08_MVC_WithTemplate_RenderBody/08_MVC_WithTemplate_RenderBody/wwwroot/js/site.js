c7cebd59416e0a9e414437efd1520373c50269f9
mikailw <mikail.wissen@gmail.com>
2022-04-06 15:02:02 +0300

    06.04.2022

--- 08_MVC_WithTemplate_RenderBody/08_MVC_WithTemplate_RenderBody/wwwroot/js/site.js
+++
@@ -1,4 +0,0 @@
-﻿////document.getElementById("#link1").addEventListener("click", function (event) {
-////	document.querySelector("#link1").classList("kırmızı");
-////	event.preventDefault();
-////});
---
+++ Week_10/08_MVC_WithTemplate_RenderBody/08_MVC_WithTemplate_RenderBody/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿////document.getElementById("#link1").addEventListener("click", function (event) {
+////	document.querySelector("#link1").classList("kırmızı");
+////	event.preventDefault();
+////});
+
+$(document).ready(function () {
+    $("li a").each(function () {
+        if ($(this).attr("href") == window.location.pathname) {
+            $(this).addClass("activeLink")
+        }
+    })
+})
+
+let a = $("li a").toArray();
+console.log(a);
