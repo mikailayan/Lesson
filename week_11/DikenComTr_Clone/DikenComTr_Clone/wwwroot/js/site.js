52ccc21b4a00a335480d5278b77256419677ebf7
mikailw <mikail.wissen@gmail.com>
2022-04-13 14:59:38 +0300

    sürdürülebilirlikten önce gelsin

--- week_11/DikenComTr_Clone/DikenComTr_Clone/wwwroot/js/site.js
+++ week_11/DikenComTr_Clone/DikenComTr_Clone/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function AcKapa() {
+    $("nav").toggle("slow");
+}
 
-// Write your JavaScript code.
+//function myFunc() {
+//    $("nav").css("display", "initial");
+//}
+
+
+//$("#menu-btn").click(function () {
+//    $("nav").toggle();
+//})
