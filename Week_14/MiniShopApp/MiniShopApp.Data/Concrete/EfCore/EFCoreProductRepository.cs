64a5bdd5382be438183f7c79ef80fa1c30d95db7
mikailw <mikail.wissen@gmail.com>
2022-05-16 14:57:09 +0300

    16.05.2022 14:56

--- Week_14/MiniShopApp/MiniShopApp.Data/Concrete/EfCore/EFCoreProductRepository.cs
+++ Week_14/MiniShopApp/MiniShopApp.Data/Concrete/EfCore/EFCoreProductRepository.cs
@@ -11,6 +11,34 @@ namespace MiniShopApp.Data.Concrete.EfCore
 {
     public class EFCoreProductRepository : EfCoreGenericRepository<Product, MiniShopContext>, IProductRepository
     {
+        private string ConvertLower(string text)
+        {
+            //İstanbul Irak Üzgün Şelaleler Satırarası
+            text = text.Replace("I", "i");//İstanbul irak Üzgün Şelaleler Satırarası
+            text = text.Replace("İ", "i");//istanbul irak Üzgün Şelaleler Satırarası
+            text = text.Replace("İ", "i");//istanbul irak Üzgün Şelaleler Satirarasi
+
+            text = text.ToLower();//istanbul irak üzgün şelaleler satirarasi
+            text = text.Replace("ç", "c");
+            text = text.Replace("ö", "o");
+            text = text.Replace("ü", "u");
+            text = text.Replace("ş", "s");
+            text = text.Replace("ğ", "g");
+            return text;
+        }
+        public List<Product> GetSearchResult(string searchString)
+        {
+            searchString = ConvertLower(searchString);
+            //Burada metodun döndürdüğü değer string ama biz linq sorgularıyla çalışırken işimize yaramıyor düzeltilecek 
+            using (var context = new MiniShopContext())
+            {
+                var products = context
+                    .Products
+                    .Where(i => i.IsApproved && (ConvertLower(i.Name).Contains(searchString) || ConvertLower(i.Description).Contains(searchString)))
+                    .ToList();
+                return products;
+            }
+        }
         public List<Product> GetHomePageProducts()
         {
             using (var context = new MiniShopContext())
@@ -57,17 +85,6 @@ namespace MiniShopApp.Data.Concrete.EfCore
             }
         }
 
-        public List<Product> GetSearchResult(string searchString)
-        {
-            searchString = searchString.ToLower();
-            using (var context = new MiniShopContext())
-            {
-                var products = context
-                    .Products
-                    .Where(i => i.IsApproved && (i.Name.ToLower().Contains(searchString) || i.Description.ToLower().Contains(searchString)))
-                    .ToList();
-                return products;
-            }
-        }
+      
     }
 }
---
+++ Week_14/MiniShopApp2/MiniShopApp.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniShopApp.Business.Abstract;
+using MiniShopApp.Entity;
+using MiniShopApp.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductService _productService;
+        private ICategoryService _categoryService;
+        public AdminController(IProductService productService, ICategoryService categoryService )
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProductList()
+        {
+            return View(_productService.GetAll());
+        }
+        public IActionResult ProductCreate()
+        {
+            ViewBag.Categories = _categoryService.GetAll();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ProductCreate(ProductModel model, int[] categoryIds) //ProductCreatede categoryIds yazdığımız için aynısını yazıyoruz
+        {
+            //Buraya validation işlemleri ile ilgili bir kontrol gelecek.
+            var product = new Product()
+            {
+                Name=model.Name,
+                Url=model.Url,
+                Price=model.Price,
+                Description=model.Description,
+                ImageUrl=model.ImageUrl,
+                IsApproved=model.IsApproved,
+                IsHome=model.IsHome
+            };
+            _productService.Create(product, categoryIds);
+            return RedirectToAction("ProductList");
+        }
+    }
+}
